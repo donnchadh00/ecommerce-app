d554f7c923f7cf42c3a2980edae6991763f3491a
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-08-04 23:29:07 +0100

    Remove unused imports

--- product-service/src/main/java/com/ecommerce/product_service/security/JwtService.java
+++ product-service/src/main/java/com/ecommerce/product_service/security/JwtService.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Date;
 import java.util.function.Function;
 
 @Service
