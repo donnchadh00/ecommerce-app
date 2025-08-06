03df96bb77d2ed20aa95d0d54df09c5e137cfe04
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-08-06 21:49:25 +0100

    Configure docker and db

--- cart-service/src/main/java/com/ecommerce/cart_service/CartServiceApplication.java
+++ cart-service/src/main/java/com/ecommerce/cart_service/CartServiceApplication.java
@@ -3,7 +3,10 @@ package com.ecommerce.cart_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.ecommerce.cart_service",
+	"com.ecommerce.common"
+})
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
