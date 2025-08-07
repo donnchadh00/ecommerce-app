d0a0439fd35613eeabb9af83516b21b86c544522
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-08-07 15:31:20 +0100

    Add missing import to cart-service

--- cart-service/src/main/java/com/ecommerce/cart_service/service/CartService.java
+++ cart-service/src/main/java/com/ecommerce/cart_service/service/CartService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.cart_service.service;
 
 import com.ecommerce.cart_service.repository.CartItemRepository;
+import com.ecommerce.cart_service.model.CartItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
