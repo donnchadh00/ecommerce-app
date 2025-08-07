62b0f54a6ab35afa107aeb884973b9c4f23dece4
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-08-07 01:31:40 +0100

    Configure payment docker, db, and dependencies

--- payment-service/src/main/java/com/ecommerce/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/ecommerce/payment_service/PaymentServiceApplication.java
@@ -3,7 +3,10 @@ package com.ecommerce.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.ecommerce.payment_service",
+	"com.ecommerce.common"
+})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
