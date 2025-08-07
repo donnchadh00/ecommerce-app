5a76ce5886d8e0cd093f4ab6be06007a0eff6996
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-08-07 19:15:19 +0100

    Configure inventory docker, dependencies and db

--- inventory-service/src/main/java/com/ecommerce/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/InventoryServiceApplication.java
@@ -3,7 +3,10 @@ package com.ecommerce.inventory_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.ecommerce.inventory_service",
+	"com.ecommerce.common"
+})
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
