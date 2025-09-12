56ef395f8e82a0163d508ff9d9bba998b66ec5a5
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-09-12 22:09:09 +0100

    feat(cache): add Caffeine cache configs for cart/order and wire dependencies

---
+++ order-service/src/main/java/com/ecommerce/order_service/config/CacheConfig.java
@@ -0,0 +1,22 @@
+package com.ecommerce.order_service.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.*;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+  @Bean
+  public CacheManager cacheManager() {
+    var mgr = new CaffeineCacheManager();
+    mgr.setCaffeine(Caffeine.newBuilder()
+        .maximumSize(10_000)
+        .expireAfterWrite(Duration.ofSeconds(60)));
+    return mgr;
+  }
+}
